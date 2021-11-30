98cbbb384a463ddf5687594175cca740b90df713
dangph <dangph1@mpos.vn>
2021-11-30 22:05:39 +0700

    add field email in Info class

--- src/main/java/com/example/messagingrabbitmq/util/Info.java
+++ src/main/java/com/example/messagingrabbitmq/util/Info.java
@@ -1,4 +1,13 @@
 package com.example.messagingrabbitmq.util;
 
 public class Info {
+    private String email;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
