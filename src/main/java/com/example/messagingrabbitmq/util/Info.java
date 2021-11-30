3b7ac6e4b7fa47846f31d5fbd4297292748a13e9
dangph <dangph1@mpos.vn>
2021-11-30 22:13:17 +0700

    add address into Info class

--- src/main/java/com/example/messagingrabbitmq/util/Info.java
+++ src/main/java/com/example/messagingrabbitmq/util/Info.java
@@ -2,6 +2,15 @@ package com.example.messagingrabbitmq.util;
 
 public class Info {
     private String email;
+    private String address;
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public String getEmail() {
         return email;
