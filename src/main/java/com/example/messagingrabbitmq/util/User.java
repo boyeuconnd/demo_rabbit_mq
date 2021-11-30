37218f4af6dc3b9bc77c51e60a11237cfc290f09
dangph <dangph1@mpos.vn>
2021-11-30 21:53:24 +0700

    add field age in User Class

--- src/main/java/com/example/messagingrabbitmq/util/User.java
+++ src/main/java/com/example/messagingrabbitmq/util/User.java
@@ -4,6 +4,15 @@ public class User {
     private String userName;
     private String password;
     private String name;
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
     public String getName() {
         return name;
